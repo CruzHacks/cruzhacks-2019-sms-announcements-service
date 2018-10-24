bb81c695b5d3ec0050f6c4a5b0c4c9087e5a601c
Kyle1668 <kyledevinobrien1@gmail.com>
2018-10-24 12:33:19 -0700

    moved file around

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.use(bodyParser.json());
+
+app.route();
