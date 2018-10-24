18055f7f1fb667109eaa2e0c51175f22590665de
Kyle1668 <kyledevinobrien1@gmail.com>
2018-10-24 15:47:30 -0700

    got GET and POST endpoints working

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { checkAPIKey } = require("./routes/middleware/authentication");
 
+// Instances
 const app = express();
 
-app.use(bodyParser.json());
+// Constants
+const PORT = process.env.PORT || 3000;
+const STAGE = process.env.STAGE || "Development";
 
-app.route();
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(checkAPIKey);
+
+// Routes
+app.get("/api/", (req, res) => {
+  console.log(req);
+  console.log(res);
+  res.json({
+    code: 200,
+    method: "GET",
+    error: false,
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.post("/api/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.listen(PORT, () => {
+  const metaData = {
+    port: PORT,
+    stage: STAGE,
+    env: process.env
+  };
+
+  console.log(`Service Listening On Port ${metaData.port}`);
+  console.log(`Enviorment Data: ${JSON.stringify(metaData, null, 2)}`);
+  console.log("\n API Launched Successfully");
+});
