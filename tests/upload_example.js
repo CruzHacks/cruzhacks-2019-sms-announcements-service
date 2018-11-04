b41a85563e8e286b669a42ac02380fda46f8b87f
Kyle1668 <kyledevinobrien1@gmail.com>
2018-11-03 21:34:14 -0700

    got example working

--- tests/upload_example.js
+++ tests/upload_example.js
@@ -7,26 +7,32 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+
 // Read in the file, convert it to base64, store to S3
 
-const resumeName = "Kyle O 'Brien Resume.pdf";
+// eslint-disable-next-line prettier/prettier
+const resumeName = "Kyle\ O\'Brien\ Resume.pdf";
 
-fs.readFile(resumeName, (err, data) => {
-  if (err) {
-    throw err;
+fs.readFile(resumeName, (fileSystemError, data) => {
+  if (fileSystemError) {
+    throw fileSystemError;
   }
 
   // eslint-disable-next-line no-buffer-constructor
   const base64data = new Buffer(data, "binary");
-  const name = "Kyle O'Brien";
-  const s3 = new AWS.S3();
 
-  s3.client.putObject(
+  const s3 = new AWS.S3();
+  const hackerName = "Kyle O'Brien";
+  const formattedName = hackerName.replace(" ", "_").toLowerCase();
+
+  s3.putObject(
     {
-      Bucket: `2019-hackers/resumes/${name.replace(" ", "_").toLowerCase()}`,
+      Bucket: `cruzhacks-2019-hackers/${formattedName}/resume/`,
       Key: resumeName,
       Body: base64data,
-      ACL: "public-read"
+      ACL: "aws-exec-read",
+      ServerSideEncryption: "AES256",
+      Tagging: "type=resume&year=cruzhacks2019"
     },
     resp => {
       console.log(resp);
@@ -34,3 +40,4 @@ fs.readFile(resumeName, (err, data) => {
     }
   );
 });
+
