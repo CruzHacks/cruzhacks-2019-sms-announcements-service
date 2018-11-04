912c981266985d829d3ccbc601dfbea3b7ab794e
Kyle1668 <kyledevinobrien1@gmail.com>
2018-11-04 12:17:22 -0800

    added better output

--- tests/upload_example.js
+++ tests/upload_example.js
@@ -1,21 +1,19 @@
 const AWS = require("aws-sdk");
 const fs = require("fs");
 
-// For dev purposes only
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: "AKIAIOSAT4F4HDM332CA",
+  secretAccessKey: "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"
 });
-
 
 // Read in the file, convert it to base64, store to S3
 
 // eslint-disable-next-line prettier/prettier
 const resumeName = "Kyle\ O\'Brien\ Resume.pdf";
 
-fs.readFile(resumeName, (fileSystemError, data) => {
-  if (fileSystemError) {
-    throw fileSystemError;
+fs.readFile(resumeName, (fileSystemerroror, data) => {
+  if (fileSystemerroror) {
+    throw fileSystemerroror;
   }
 
   // eslint-disable-next-line no-buffer-constructor
@@ -25,19 +23,24 @@ fs.readFile(resumeName, (fileSystemError, data) => {
   const hackerName = "Kyle O'Brien";
   const formattedName = hackerName.replace(" ", "_").toLowerCase();
 
-  s3.putObject(
+  s3.upload(
     {
-      Bucket: `cruzhacks-2019-hackers/${formattedName}/resume/`,
+      Bucket: `cruzhacks-2019-hackers/${formattedName}/resume`,
       Key: resumeName,
       Body: base64data,
       ACL: "aws-exec-read",
       ServerSideEncryption: "AES256",
       Tagging: "type=resume&year=cruzhacks2019"
     },
-    resp => {
-      console.log(resp);
-      console.log("Successfully uploaded package.");
+    (error, response) => {
+      if (error) {
+        console.log("erroror Uploading To S3");
+        console.log(error);
+      } else {
+        //   Prints location in S3
+        console.log(response.Location);
+        console.log("Successfully uploaded package.");
+      }
     }
   );
 });
-
